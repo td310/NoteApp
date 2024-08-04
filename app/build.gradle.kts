610c9c20519da6fe6404e300a2b2ff1a10cb6fb1
Tai <ndtai0912@gmail.com>
2024-08-04 18:57:20 +0700

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,20 +92,7 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.8.1")
 
 
-    // optional - RxJava2 support for Room
-    implementation("androidx.room:room-rxjava2:$room_version")
 
-    // optional - RxJava3 support for Room
-    implementation("androidx.room:room-rxjava3:$room_version")
-
-    // optional - Guava support for Room, including Optional and ListenableFuture
-    implementation("androidx.room:room-guava:$room_version")
-
-    // optional - Test helpers
-    testImplementation("androidx.room:room-testing:$room_version")
-
-    // optional - Paging 3 Integration
-    implementation("androidx.room:room-paging:$room_version")
 
 
 
--- app/src/main/java/com/example/noteapp/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/noteapp/fragment/HomeFragment.kt
@@ -61,7 +61,7 @@ class HomeFragment : Fragment(R.layout.fragment_home), SearchView.OnQueryTextLis
     private fun setupHomeRecyclerView() {
         noteAdapter = NoteAdapter()
         binding.homeRecyclerView.apply {
-            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.VERTICAL)
             setHasFixedSize(true)
             adapter = noteAdapter
         }
